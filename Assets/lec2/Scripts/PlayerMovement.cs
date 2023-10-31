c8a0bd0f140d4c5b8246eed0aa8176a0608019ad
eren-erdgn <109946565+eren-erdgn@users.noreply.github.com>
2023-10-31 12:08:39 +0300

    switch road logic implemented

--- Assets/lec2/Scripts/PlayerMovement.cs
+++ Assets/lec2/Scripts/PlayerMovement.cs
@@ -3,51 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG;
 using DG.Tweening;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float _speed = 0f;
+    [SerializeField] float _runSpeed;
+    [SerializeField] float _switchSpeed;
     private bool _tapToStart = false;
+    private int[] _roadSections;
+    private int _roadIndex = 1;
+    private Animator _animator;
 
     private void Awake()
     {
-
+        _animator = GetComponent<Animator>();
+        _roadSections = new int[3];
+        _roadSections[0] = -2;
+        _roadSections[1] = 0;
+        _roadSections[2] = 2;
     }
     // Start is called before the first frame update
     void Start()
     {
+        _roadIndex = 1;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if(_tapToStart == false) return;
-        transform.Translate(new Vector3(0, 0, 1f) * _speed * Time.deltaTime);
+        transform.Translate(new Vector3(0, 0, 1f) * _runSpeed * Time.deltaTime);
         
         
     }
     private void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             print("ENTER PRESSED");
-            GetComponent<Animator>().SetBool("RunStart", true);
+            _animator.SetBool("RunStart", true);
             _tapToStart=true;
         }
         if(Input.GetKeyDown (KeyCode.A))
         {
-            if(transform.position.x > -1)
-            {
-                transform.DOMoveX(transform.position.x - 1.5f, _speed * Time.deltaTime);
-            }
+            if (_roadIndex == 0) return;
+            _roadIndex -= 1;
+            _animator.SetBool("RunLeft", true);
+            _animator.SetBool("RunRight", false);
+            
+
+            transform.DOMoveX(_roadSections[_roadIndex], _switchSpeed)
+                .OnComplete(() =>
+                {
+                    _animator.SetBool("RunLeft", false);
+                });
         }
         if(Input.GetKeyDown (KeyCode.D))
         {
-            if (transform.position.x < 1)
-            {
-                transform.DOMoveX(transform.position.x + 1.5f, _speed * Time.deltaTime);
-                
-            }
+            if (_roadIndex == 2) return;
+            _roadIndex += 1;
+            _animator.SetBool("RunLeft", false);
+            _animator.SetBool("RunRight", true);
+            transform.DOMoveX(_roadSections[_roadIndex], _switchSpeed)
+                .OnComplete(() =>
+                {
+                    _animator.SetBool("RunRight", false);
+                });
         }
+
     }
 }
